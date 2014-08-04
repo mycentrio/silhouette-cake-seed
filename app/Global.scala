5e2be5e7efb5f605cf581cbf633a7f38ebe9dd08
yzernik <yzernik@gmail.com>
2014-08-03 19:07:16 -0700

    cleaned up Global

--- app/Global.scala
+++ app/Global.scala
@@ -5,28 +5,15 @@
 import com.mohiva.play.silhouette.core.Logger
 import com.mohiva.play.silhouette.core.SecuredSettings
 
-import controllers.MyApplication
 import play.api.Application
 import play.api.GlobalSettings
 import utils.EnvironmentModule
-import play.api.mvc.Controller
 
-trait DynamicGlobal extends GlobalSettings with SecuredSettings with EnvironmentModule with Logger {
-
-  //val controllers: Map[Class[_ <: Controller], _ <: Controller] = Map(classOf[MyApplication] -> new MyApplication())
+object Global extends GlobalSettings with SecuredSettings with EnvironmentModule with Logger {
 
   override def onStart(app: Application) {
     play.api.Logger.info("Application has started")
     play.api.Logger.info("env: " + env)
   }
 
-  /*
-  override def getControllerInstance[A](controllerClass: Class[A]): A = controllers.get(controllerClass.asInstanceOf[Class[_ <: Controller]]) match {
-    case Some(controller) => controller.asInstanceOf[A]
-    case _ => super.getControllerInstance(controllerClass)
-  }
-  */
-
 }
-
-object Global extends DynamicGlobal
